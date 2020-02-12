651972f1d22cecb80a4c8e4a90c77f52c702ffbc
waltercraig <walterrosscraig@gmail.com>
2020-02-12 15:27:54 +0000

    Added basic layout to default page and post

--- single.php
+++ single.php
@@ -10,28 +10,33 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div id="primary" class="section">
+		<div class="container">
+			<div class="row">
+				<main id="main" class="col-xs-12 col-md-6 offset-md-1">
+					<?php
+					while ( have_posts() ) :
+						the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+						get_template_part( 'template-parts/content', get_post_type() );
 
-			get_template_part( 'template-parts/content', get_post_type() );
+						the_post_navigation();
 
-			the_post_navigation();
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
+					endwhile; // End of the loop.
+					?>
+				</main><!-- #main -->
+				<aside class="col-xs-12 col-md-4 offset-md-1">
+					<?php get_sidebar(); ?>
+				</aside>
+			</div><!--/-row-->
+		</div><!--/-container-->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
